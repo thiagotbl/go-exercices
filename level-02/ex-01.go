009bb11d1899959ba7e1debd44c3704b302bc792
Thiago Lopes <lopestbl@gmail.com>
2020-06-18 20:32:30 -0300

    Add exercise level-02/01

---
+++ level-02/ex-01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 327
+
+	fmt.Printf("in decimal: %d\n", number)
+	fmt.Printf("in binary: %b\n", number)
+	fmt.Printf("in hexadecimal: %#x\n", number)
+}
